990b750a50d948ce708e856ccb1f2fa4a5b7a5ed
kdando <kvdando@gmail.com>
2024-09-22 19:00:56 +0100

    refactoring components for MUI

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,15 +1,29 @@
-import Link from 'next/link';
+// components/Navbar.js
 
+import { Button, AppBar, Toolbar, Typography } from '@mui/material';
+import Link from 'next/link';
 
 const Navbar = () => {
     return (
-        <nav>
-            <Link href="/">Welcome</Link>
-            <Link href="/my-collection">My Collection</Link>
-            <Link href="/search">Search</Link>
-            <Link href="/signup">Sign Up/Log In</Link>
-        </nav>
+        <AppBar position="static">
+            <Toolbar>
+                <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+                    My App
+                </Typography>
+                <Button color="inherit" component={Link} href="/">
+                    Welcome
+                </Button>
+                <Button color="inherit" component={Link} href="/my-collection">
+                    My Collection
+                </Button>
+                <Button color="inherit" component={Link} href="/search">
+                    Search
+                </Button>
+                <Button color="inherit" component={Link} href="/signup">
+                    Sign Up/Log In
+                </Button>
+            </Toolbar>
+        </AppBar>
     );
 };
-
 export default Navbar;
--- components/ObjectCard.js
+++ components/ObjectCard.js
@@ -1,15 +1,16 @@
 // components/ObjectCard.js
 
-import React, { useState, useContext } from 'react';
-import CustomModal from './Modal';
+import React, { useState } from 'react';
+import { useContext } from 'react';
 import { CollectionContext } from '../contexts/CollectionContext';
-import styles from '../styles/ObjectCard.module.css';
+import { Card, CardMedia, CardContent, CardActions, Button, Typography } from '@mui/material';
+import CustomModal from './Modal';
 
-const ObjectCard = ({ object, onImageClick }) => {
+const ObjectCard = ({ object }) => {
     const [isModalOpen, setIsModalOpen] = useState(false);
 
     const { collection, addToCollection, removeFromCollection } = useContext(CollectionContext);
-    const isInCollection = collection.some(obj => obj.objectID === object.objectID);
+    const isInCollection = collection.some((obj) => obj.objectID === object.objectID);
 
     const openModal = () => {
         setIsModalOpen(true);
@@ -20,24 +21,41 @@ const ObjectCard = ({ object, onImageClick }) => {
     };
 
     return (
-        <div className={styles.card}>
+        <Card sx={{ maxWidth: 345 }}>
             {object.primaryImageSmall ? (
-                <img src={object.primaryImageSmall} alt={object.title} onClick={openModal} style={{ cursor: 'pointer' }} />
+                <CardMedia
+                    component="img"
+                    height="194"
+                    image={object.primaryImageSmall}
+                    alt={object.title}
+                    onClick={openModal}
+                    style={{ cursor: 'pointer' }}
+                />
             ) : (
-                <p onClick={openModal}>No image available</p>
+                <CardContent>
+                    <Typography variant="body2" color="text.secondary">
+                        No image available
+                    </Typography>
+                </CardContent>
             )}
-            <h2>{object.title}</h2>
-            {isInCollection ? (
-                <button onClick={() => removeFromCollection(object.objectID)}>Remove from Collection</button>
-            ) : (
-                <button onClick={() => addToCollection(object)}>Add to Collection</button>
-            )}
-            <CustomModal
-                isOpen={isModalOpen}
-                onRequestClose={closeModal}
-                content={object}
-            />
-        </div>
+            <CardContent>
+                <Typography gutterBottom variant="h5" component="div">
+                    {object.title}
+                </Typography>
+            </CardContent>
+            <CardActions>
+                {isInCollection ? (
+                    <Button size="small" onClick={() => removeFromCollection(object.objectID)}>
+                        Remove from Collection
+                    </Button>
+                ) : (
+                    <Button size="small" onClick={() => addToCollection(object)}>
+                        Add to Collection
+                    </Button>
+                )}
+            </CardActions>
+            <CustomModal isOpen={isModalOpen} onRequestClose={closeModal} content={object} />
+        </Card>
     );
 };
 
