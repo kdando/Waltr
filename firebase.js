d17da0137ec175c829389c7a065c3de89265c541
kdando <kvdando@gmail.com>
2024-09-28 20:50:32 +0100

    added firestore db

---
+++ firebase.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyCaotb9den76lsC1sGTaNmgoDdGb5DdSLI",
+    authDomain: "waltr-authentication.firebaseapp.com",
+    projectId: "waltr-authentication",
+    storageBucket: "waltr-authentication.appspot.com",
+    messagingSenderId: "577462383007",
+    appId: "1:577462383007:web:f4fe0b5d66d7c8f988cdaa"
+};
+
+// Initialize Firebase
+const app = getApps().length ? getApp() : initializeApp(firebaseConfig);
+export { app };
+
--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,3 +1,5 @@
+import { FirestoreAdapter } from "@auth/firebase-adapter";
+import { cert } from "firebase-admin/app";
 import NextAuth from "next-auth"
 import GoogleProvider from "next-auth/providers/google";
 
@@ -15,7 +17,14 @@ export const authOptions = {
                 }
             }
         })
-    ]
+    ],
+    adapter: FirestoreAdapter({
+        credential: cert({
+            projectId: process.env.AUTH_FIREBASE_PROJECT_ID,
+            clientEmail: process.env.AUTH_FIREBASE_CLIENT_EMAIL,
+            privateKey: process.env.AUTH_FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+        }),
+    }),
 }
 
 export default NextAuth(authOptions)
